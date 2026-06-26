0e6c6122f5e0ce74ac9260dd53b15b18371c3571
miolulu <miolulu@users.noreply.github.com>
2026-06-26 17:34:57 +0800

    Updates

--- assets/BufferResource-y3KbyMGw.js
+++ assets/BufferResource-y3KbyMGw.js
@@ -1,4 +1,4 @@
-import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-BUwHqiXx.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-DGe_LtFI.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
