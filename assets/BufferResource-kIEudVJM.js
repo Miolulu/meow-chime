0676272ed1ff11376ac2374325d221339399f5fc
miolulu <miolulu@users.noreply.github.com>
2026-06-29 14:04:01 +0800

    Updates

--- assets/BufferResource-kIEudVJM.js
+++ assets/BufferResource-kIEudVJM.js
@@ -1,4 +1,4 @@
-import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-B4tcTW26.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-DvmIObB7.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
