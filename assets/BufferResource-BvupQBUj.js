f03deccbbb35e421cded0317b4f6e2576684e743
miolulu <miolulu@users.noreply.github.com>
2026-06-29 11:41:58 +0800

    Updates

--- assets/BufferResource-BvupQBUj.js
+++ assets/BufferResource-BvupQBUj.js
@@ -1,4 +1,4 @@
-import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-aHvAMSw8.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-D8xenshV.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
